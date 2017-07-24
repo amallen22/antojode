d0b86d4c97b736beb2c5787698cd670cdb4fe120
Alex Mallen <alex.mallengines@gmail.com>
2017-07-24 16:31:14 +0200

    add localService in dataService. Change index.js routing to /home/:id

--- js/routes/categories/controller.js
+++ js/routes/categories/controller.js
@@ -1,9 +1,11 @@
 angular.module('mainApp')
-.controller('categoriesController', function ($scope, dataService) {
+.controller('categoriesController', function ($scope, $routeParams, dataService) {
   $scope.section = 'Categories'
 
-  dataService.getConfig()
-	.then(function (config) {
-  console.log(config)
-})
+  var categoryId = $routeParams.id
+
+  // dataService.getCategoryId(categoryId)
+  // .then(function (config) {
+  // 	console.log(config)
+  // })
 })
--- js/services/dataService.js
+++ js/services/dataService.js
@@ -1,13 +1,33 @@
 angular.module('mainApp')
 
+  // promise foursquare
   .factory('dataService', function ($http) {
     var clientId = 'NOMTFMQBL1FV34UIYMEC0AOM0F11X2D0NXQOXTB15X4XHXRT'
     var clientSecret = 'I21LTBGGQSNUZXIR3AUSSZTVN0ASR15C43JDA4TGFMGHB3VC'
+
     function getConfig () {
       var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20130815&near=barcelona&query=desayuno, salado'
       return $http.get(url)
     }
     return {
       getConfig: getConfig
+    }
+  })
+
+  // promise local
+  .factory('localService', function () {
+    var localData = [
+      {
+        id: 01,
+        title: "desayuno",
+        subCategory: {
+          type01: 'dulce',
+          type02: 'salado' 
+        }
+      }
+    ] 
+   
+    return {
+      localData: localData
     }
   })
