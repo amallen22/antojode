ceab60ca07e85a9e761a8178e48688a2a70d5690
aleixordeig <aleix.ordeig@gmail.com>
2017-07-24 15:25:53 +0200

    step 1 desayunos dulces

--- js/services/dataService.js
+++ js/services/dataService.js
@@ -4,7 +4,7 @@ angular.module('mainApp')
   var clientId = 'NOMTFMQBL1FV34UIYMEC0AOM0F11X2D0NXQOXTB15X4XHXRT'
   var clientSecret = 'I21LTBGGQSNUZXIR3AUSSZTVN0ASR15C43JDA4TGFMGHB3VC'
   function getConfig () {
-    var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20130815&near=barcelona&query=barcos'
+    var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20130815&near=barcelona&query=dulce'
     return $http.get(url)
   }
   return {
