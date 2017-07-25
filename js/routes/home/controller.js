8087c9c2b9855f4e3bf9015715a7f985add2f575
Alex Mallen <alex.mallengines@gmail.com>
2017-07-25 10:57:29 +0200

    Resolve conflict

--- js/routes/home/controller.js
+++ js/routes/home/controller.js
@@ -3,6 +3,7 @@ angular.module('mainApp')
 .controller('homeController', function ($scope, localData) {
   $scope.section = 'Vissont'
 
+  $scope.localData = localData
   // added marc
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function (position) {
