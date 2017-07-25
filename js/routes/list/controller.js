a4fb1080afb71811ad9dce641056912ade01d24b
Marc Vergara <marc.vergara.f@gmail.com>
2017-07-25 20:44:05 +0200

    maketa home

--- js/routes/list/controller.js
+++ js/routes/list/controller.js
@@ -1,22 +1,30 @@
 angular.module('mainApp')
-    .controller('listController', function($scope, $routeParams, dataService, localData) {
-        $scope.section = "LIST"
+.controller('listController', function($scope, $routeParams, dataService, localData) {
+  $scope.section = "LIST"
 
-         var catId = $routeParams.catId
-         var subId = $routeParams.subId
-         
-         var queryCategory = localData[catId].title
-         var querySubCategory = localData[catId].subCategory[subId].type
-         
-         // esta query tiene que llegar al dataService para recojer parametro
-         var query = queryCategory + " " + querySubCategory
-         
-		
-		dataService.getConfig(query)
-		.then(function (config) {
-			console.log(config)
-		})
+  var catId = $routeParams.catId
+  var subId = $routeParams.subId
 
-        
+  var queryCategory = localData[catId].title
+  var querySubCategory = localData[catId].subCategory[subId].type
 
-    })
+  // esta query tiene que llegar al dataService para recojer parametro
+  var query = queryCategory + " " + querySubCategory
+
+
+  dataService.getConfig(query)
+  .then(function (config) {
+   console.log(config)
+ })
+
+  // if (navigator.geolocation) {
+  //   navigator.geolocation.getCurrentPosition(function (position) {
+  //     $scope.$apply(function () {
+  //       $scope.position = position
+  //       console.log(position)
+  //     })
+  //   })
+  // }
+
+
+})
