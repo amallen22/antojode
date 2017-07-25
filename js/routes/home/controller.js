7e07fa4e5dcfc71805cf600e1a1be9cbb6733cf7
Marc Vergara <marc.vergara.f@gmail.com>
2017-07-25 09:36:06 +0200

    added geolocation

--- js/routes/home/controller.js
+++ js/routes/home/controller.js
@@ -1,5 +1,16 @@
 angular.module('mainApp')
-.controller('homeController', function($scope, dataService){
+.controller('homeController', function($scope, localData){
 	$scope.section = "Vissont"
+
+	// added marc
+	if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position){
+      $scope.$apply(function(){
+        $scope.position = position;
+        console.log(position)
+      });
+    });
+  }
+  // end added marc
 	
 })
