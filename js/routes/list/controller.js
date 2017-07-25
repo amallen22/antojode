a9ce80f7b52c6d6721620085ec4189b40459573b
aleixordeig <aleix.ordeig@gmail.com>
2017-07-25 08:58:23 +0200

    First Changes

--- js/routes/list/controller.js
+++ js/routes/list/controller.js
@@ -2,9 +2,27 @@ angular.module('mainApp')
 .controller('listController', function($scope, dataService){
 	$scope.section = "LIST"
 
-	dataService.getConfig()
+	dataService.getSubId(subId)
 	.then(function(config){
 		console.log(config)
 	})	
 
 })
+
+
+//// (function () {
+//   $(document).on('albumSelection', function (e, idAlbum) {
+//     SpotifyService.getTracks(idAlbum)
+//       .then(function (oData) {
+//         var liTemplate = '<li><a target="_blank" href="<%URL%>"><%NAME%></a></li>'
+//         var aLisTracks = oData.items.map(function (oTrack) {
+//           return liTemplate
+//             .replace('<%URL%>', oTrack.preview_url)
+//             .replace('<%NAME%>', oTrack.name)
+//         })
+
+//         $('#tracks-list ul').html(aLisTracks.join(''))
+//         $('#tracks-list').removeClass('hidden')
+//       })
+//   })
+// })()
--- js/services/dataService.js
+++ js/services/dataService.js
@@ -1,17 +1,13 @@
 angular.module('mainApp')
 
   .factory('dataService', function ($http) {
-
-  var clientId = 'NOMTFMQBL1FV34UIYMEC0AOM0F11X2D0NXQOXTB15X4XHXRT'
-  var clientSecret = 'I21LTBGGQSNUZXIR3AUSSZTVN0ASR15C43JDA4TGFMGHB3VC'
-  function getConfig () {
-
-    var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20130815&near=barcelona&query=dulce'
-
-    var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20130815&near=barcelona&query=desayuno, salado'
-    return $http.get(url)
-  }
-  return {
-    getConfig: getConfig
-  }
-})
+    var clientId = 'NOMTFMQBL1FV34UIYMEC0AOM0F11X2D0NXQOXTB15X4XHXRT'
+    var clientSecret = 'I21LTBGGQSNUZXIR3AUSSZTVN0ASR15C43JDA4TGFMGHB3VC'
+    function getConfig () {
+      var url = 'https://api.foursquare.com/v2/venues/explore?client_id=' + clientId + '&client_secret=' + clientSecret + '&v=20130815&near=barcelona&query=desayuno, salado'
+      return $http.get(url)
+    }
+    return {
+      getConfig: getConfig
+    }
+  })
