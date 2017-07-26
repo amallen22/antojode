46e3bef14503f24061e34dbc577c7f15bb29d0f4
Alex Mallen <alex.mallengines@gmail.com>
2017-07-26 17:07:54 +0200

    details files

--- js/routes/categories/controller.js
+++ js/routes/categories/controller.js
@@ -1,6 +1,6 @@
 angular.module('mainApp')
 
-.controller('categoriesController', function ($scope, $rootScope, $routeParams, localData, dataService) {
+.controller('detailController', function ($scope, $rootScope, $routeParams, localData, dataService) {
   var categoryId = $routeParams.catId
   $scope.categoryId = $routeParams.catId
   $scope.localData = localData[categoryId]
