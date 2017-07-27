fbb348213da27ee44c5e4b78f864a34f5953f40a
Alex Mallen <alex.mallengines@gmail.com>
2017-07-27 12:25:54 +0200

    page detail-> dynamic background image url from controller

--- js/routes/detail/controller.js
+++ js/routes/detail/controller.js
@@ -2,14 +2,19 @@ angular.module('mainApp')
 
 .controller('detailController', function ($scope, $routeParams, dataService) {
   var detId = $routeParams.detId
-  // $scope.catId = $routeParams.catId
-  // $scope.subId = $routeParams.subId
+  $scope.catId = $routeParams.catId
+  $scope.subId = $routeParams.subId
 
   $scope.section = 'Antojo de '
 
   dataService.getVenueDetails(detId)
     .then(function (detId) {
+    	var imgPre = detId.data.response.venue.bestPhoto.prefix
+    	var imgSuf = 'original' + detId.data.response.venue.bestPhoto.suffix
+    	var imgUrl = imgPre + imgSuf
+
       $scope.venueDetail = detId.data.response.venue
-      console.log(detId)
+      $scope.imgUrl = imgUrl
+      console.log(imgUrl)
     })
 })
